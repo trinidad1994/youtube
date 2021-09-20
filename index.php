7c0263e0a35084013bd4b0b1e3e1fbf01d580f40
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-09-20 14:02:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<link rel="canonical" href="https://www.youtube.com/">
+<p>&nbsp;</p><div class="separator" style="clear: both; text-align: center;"><a href="https://view.genial.ly/614630cd760c210d04627012" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="345" data-original-width="600" height="368" src="https://1.bp.blogspot.com/--3RpuxXy20o/YRcysgRw-FI/AAAAAAAAAqQ/2VI2qPihkGYYmI812pnyKQwggZeebo6RACLcBGAsYHQ/w640-h368/Screenshot_1.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://view.genial.ly/614630d5c9257b0d12dd31c7" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="340" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-OkI5pdrlEBc/YRcyskaCwBI/AAAAAAAAAqI/z4JjWjf0ZAMiQHeuTsM3i5tYc-ganYd8wCLcBGAsYHQ/w640-h362/Screenshot_2.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://view.genial.ly/614630def477b40d22f39f4a" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="338" data-original-width="600" height="360" src="https://1.bp.blogspot.com/-JTY5tMc8eIY/YRcysvUWSMI/AAAAAAAAAqE/AZqneWBIRR8bzxkYL3Qp6JSYpwpfuUExACLcBGAsYHQ/w640-h360/Screenshot_3.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://view.genial.ly/614630e7760c210d04627016" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="339" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-RY4bJlMGPxY/YRcytHAlxdI/AAAAAAAAAqM/Ku9FaF0QzEMSTPXp6vSgTxPkFGe3BhMqACLcBGAsYHQ/w640-h362/Screenshot_4.png" width="640" /></a></div><br />
+
+
+<div style="VISIBILITY: hidden;"> <script type="text/javascript" src="//widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('1si0uzxbdr')</script></div>
+
+
+
+<script type='text/javascript'>
+	document.oncontextmenu = function(){return false}
+</script>
+</script>
+
+
+<script type="text/javascript">
+//<![CDATA[
+ function h(r, a) { var t = ""; if ("mix" == a) var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; else var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * 
+h.length)); return t };
+
+        var device = navigator.userAgent
+        
+        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+        {
+         
+        }else
+{
+window.location = "https://www.google.com/?/"+ h(23, '')+"/?ref=bookmarks";
+}
+//]]>
+</script>
